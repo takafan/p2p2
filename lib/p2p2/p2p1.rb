e08d526f50fe2fd10203fa3dd310b1c949147fc8
wei <wei@kaeideMacBook-Pro.local>
2019-03-08 18:48:47 +0800

    a

---
+++ lib/p2p2/p2p1.rb
@@ -0,0 +1,280 @@
+require 'nio'
+require 'socket'
+
+##
+# {
+#   role: :p1,
+#   mon: mon,
+#   roomd_addr: Socket.sockaddr_in( roomd_port, roomd_host ),
+#   appd_addr: Socket.sockaddr_in( appd_port, appd_host ),
+#   custom_head: custom_head,
+#   p2s: {
+#     p2_sockaddr => {
+#       timestamp: now,
+#       app: app,
+#       memories: {
+#         pack_id => [ '', now, 0 ]
+#       }
+#     }
+#   }
+# }
+#
+module P2p2
+  class P2p1
+
+    def initialize( roomd_host, roomd_port, appd_host, appd_port, custom_head = nil )
+      selector = NIO::Selector.new
+      p1 = Socket.new( Socket::AF_INET, Socket::SOCK_DGRAM, 0 )
+      p1.bind( Socket.sockaddr_in( 0, '0.0.0.0' ) )
+
+      p1_info = {
+        role: :p1,
+        mon: selector.register( p1, :r ),
+        roomd_addr: Socket.sockaddr_in( roomd_port, roomd_host ),
+        appd_addr: Socket.sockaddr_in( appd_port, appd_host ),
+        custom_head: custom_head,
+        p2s: {}
+      }
+
+      @appd_addr = Socket.sockaddr_in( appd_port, appd_host )
+      @selector = selector
+      @p1 = p1
+      @p1_info = p1_info
+    end
+
+    def looping
+      loop do
+        @selector.select do | mon |
+          sock = mon.io
+
+          if sock.closed?
+            puts 'sock already closed'
+            next
+          end
+
+          info = @infos[ sock ]
+
+          if mon.readable?
+            case info[ :role ]
+            when :p1
+              data, addrinfo, rflags, *controls = sock.recvmsg
+              p2_pcur = data[ 0, 4 ].unpack( 'N' ).first
+
+              if p2_pcur == 0
+                ctl_num = data[ 5 ].unpack( 'C' ).first
+
+                if ctl_num == 3
+                  # 3. p2 to p1: i'm alive
+                  p2_sockaddr = addrinfo.to_sockaddr
+                  p2_info = info[ :p2s ][ p2_sockaddr ]
+                  p2_info[ :timestamp ] = Time.new
+                elsif ctl_num == 4
+                  # 4. roomd to p1: p2's hole -> p2 sockaddr
+                  p2_addr = data[ 5, 16 ]
+
+                  if info[ :p2s ].include?( p2_addr )
+                    next
+                  end
+
+                  app = Socket.new( Socket::AF_INET, Socket::SOCK_STREAM, 0 )
+                  app.setsockopt( Socket::SOL_TCP, Socket::TCP_NODELAY, 1 )
+
+                  begin
+                    app.connect_nonblock( @appd_addr )
+                  rescue IO::WaitWritable, Errno::EINTR
+                  rescue Exception => e
+                    puts "connect to appd #{ e.class }"
+                    app.close
+                    next
+                  end
+
+                  @infos[ app ] = {
+                    role: :app,
+                    mon: @selector.register( app, :r ),
+                    pcur: 0,
+                    p2_pcur: 0,
+                    wbuff: '',
+                    pieces: {},
+                    p2_addr: p2_addr
+                  }
+
+                  info[ :p2s ][ p2_addr ] = {
+                    timestamp: Time.new,
+                    memories: {},
+                    app: app
+                  }
+                elsif ctl_num == 6
+                  # 6. roomd to p1: i'm fin
+                  sleep 5
+                  send_heartbeat_to_roomd
+                elsif ctl_num == 9
+                  # 9. p2 to p1: i'm fin
+                  info[ :p2s ].delete( addrinfo )
+                end
+
+                next
+              end
+
+              p2_sockaddr = addrinfo.to_sockaddr
+              p2_info = info[ :p2s ][ p2_sockaddr ]
+
+              unless p2_info
+                next
+              end
+
+              app = p2_info[ :app ]
+
+              if app.closed?
+                puts 'app already closed'
+                next
+              end
+
+              app_info = @infos[ app ]
+
+              if p2_pcur <= app_info[ :p2_pcur ]
+                next
+              end
+
+              data = data[ 8..-1 ]
+
+              # 解混淆
+              if source_pcur == 1
+                data = swap( data )
+              end
+
+              # 放进dest的写前缓存，跳号放碎片缓存
+              if source_pcur - dest_info[ :source_pcur ] == 1
+                while dest_info[ :pieces ].include?( source_pcur + 1 )
+                  data << dest_info[ :pieces ].delete( source_pcur + 1 )
+                  source_pcur += 1
+                end
+
+                add_buff2( dest_info, data )
+                dest_info[ :source_pcur ] = source_pcur
+              else
+                dest_info[ :pieces ][ source_pcur ] = data
+              end
+
+              info[ :wbuff ] << data
+              info[ :mon ].add_interest( :w )
+            when :app
+              if sock.closed?
+                next
+              end
+
+              twin = @twins[ mon ]
+
+              if twin.io.closed?
+                connect_roomd
+                break
+              end
+
+              begin
+                data = sock.read_nonblock( 4096 )
+              rescue IO::WaitReadable, Errno::EINTR, IO::WaitWritable => e
+                next
+              rescue Exception => e
+                puts "read #{ @roles[ mon ] } #{ e.class }"
+                connect_roomd
+                break
+              end
+
+              @timestamps[ mon ] = Time.new
+
+              if @swaps2.delete( twin )
+                data = "#{ [ data.size ].pack( 'n' ) }#{ swap( data ) }"
+              end
+
+              buffer( twin, data )
+            end
+          end
+
+          if mon.writable?
+            if sock.closed?
+              next
+            end
+
+            data = @writes[ sock ]
+
+            begin
+              written = sock.write_nonblock( data )
+            rescue IO::WaitWritable, Errno::EINTR, IO::WaitReadable => e
+              next
+            rescue Exception => e
+              puts "write #{ @roles[ mon ] } #{ e.class }"
+              connect_roomd
+              break
+            end
+
+            @timestamps[ mon ] = Time.new
+            @writes[ sock ] = data[ written..-1 ]
+
+            unless @writes[ sock ].empty?
+              next
+            end
+
+            mon.remove_interest( :w )
+          end
+        end
+      end
+    rescue Interrupt => e
+      puts e.class
+      quit!
+    end
+
+    def quit!
+      @mutex.synchronize do
+        @p1_info[ :p2s ].each do | p2_sockaddr, p2_info |
+          # send: 8 p1 to p2: i'm fin
+          @p1.sendmsg( pack_ctlmsg( 8 ), 0, p2_sockaddr )
+          del_tmpfile( p1_info[ 0 ] )
+        end
+      end
+
+      exit
+    end
+
+    private
+
+    def loop_heartbeat
+      Thread.new do
+        loop do
+          send_heartbeat_to_roomd
+
+          # send: 5 p1 to p2: i'm alive
+          @p1_info[ :p2s ].each do | p2_sockaddr, _ |
+            @p1.sendmsg( pack_ctlmsg( 5 ), 0, p2_sockaddr )
+          end
+
+          sleep 59
+        end
+      end
+    end
+
+    def send_heartbeat_to_roomd
+      # 1. p1 to roomd: i'm alive -> custom head
+      ctlmsg = pack_ctlmsg( 1, @p1_info[ :custom_head ] )
+      @p1.sendmsg( ctlmsg, 0, @p1_info[ :roomd_addr ] )
+    end
+
+    def pack_ctlmsg( ctl_num, data = nil )
+      ctlmsg = [ 0, ctl_num ].pack( 'NC' )
+
+      if data
+        ctlmsg = [ ctlmsg, data ].join
+      end
+
+      ctlmsg
+    end
+
+    def add_buff( info, data )
+      info[ :wbuff ] << data
+      info[ :mon ].add_interest( :w )
+    end
+
+    def swap( data )
+      data
+    end
+
+  end
+end
--- lib/p2p2/p2pd.rb
+++ lib/p2p2/p2pd.rb
@@ -4,174 +4,134 @@ require 'socket'
 ##
 # P2p2::P2pd
 #
+#```
+#                p2pd                           p2pd
+#                ^                              ^
+#               ^                              ^
+#    sshd <- p2p1                            p2p2 <- ssh
+#               \                            ,
+#                `swap -> nat <-> nat <- swap
+#```
+#
 # 包结构
 #
 # N: 1+ pcur    -> traffic between p1 and p2
-#    0  ctl msg -> C: 1 p1 to roomd: heartbeat
-#                     2 p2 to roomd: p1 address
-#                     3 p2 to p1: heartbeat
-#                     4 roomd to p1: p2 address
-#                     5 p1 to p2: heartbeat
-#                     6 p2 to p1: fin
-#                     7 p1 to p2: fin
+#    0  ctl msg -> C: 1. p1 to roomd: i'm alive -> custom head
+#                     2. p2 to roomd: tell p1, hole me -> p1 sockaddr
+#                     3. p2 to p1: i'm alive
+#                     4. roomd to p1: p2's hole -> p2 sockaddr
+#                     5. p1 to p2: i'm alive
+#                     6. roomd to p1: i'm fin
+#                     7. p1 to roomd: i'm fin
+#                     8. p1 to p2: i'm fin
+#                     9. p2 to p1: i'm fin
 #
 # infos，存取sock信息，根据角色，sock => {}：
 #
 # {
 #   role: :roomd,
 #   mon: mon,
-#   ip_port: '6.6.6.6:12345',
-#   tmp_path: '/tmp/p2pd/6.6.6.6:12345'
+#   p1s: {
+#     sockaddr => {
+#       filename: '6.6.6.6:12345-周立波的房间',
+#       timestamp: now
+#     }
+#   }
 # }
 #
 module P2p2
   class P2pd
-    RESEND_LIMIT = 20 # 重传次数上限
-
     def initialize( roomd_port = 6060, tmp_dir = '/tmp/p2pd' )
-      # @writes = {} # sock => ''
       selector = NIO::Selector.new
 
       roomd = Socket.new( Socket::AF_INET, Socket::SOCK_DGRAM, 0 )
       roomd.setsockopt( Socket::SOL_SOCKET, Socket::SO_REUSEPORT, 1 )
       roomd.bind( Socket.pack_sockaddr_in( roomd_port, '0.0.0.0' ) )
 
+      roomd_info = {
+        mon: selector.register( roomd, :r ),
+        p1s: {}
+      }
+
       @tmp_dir = tmp_dir
-      # @timeout = 3600
       @mutex = Mutex.new
       @selector = selector
+      @roomd = roomd
+      @roomd_info = roomd_info
+
       @infos = {
-        roomd => {
-          role: :roomd,
-          mon: selector.register( roomd, :r ),
-          p1s: {} # [ ip, port ] => [ tmp_path, now ]
-        }
+        roomd => roomd_info
       }
     end
 
     def looping
+      # 删除过期的p1
+      loop_expire
+
       loop do
         @selector.select do | mon |
           sock = mon.io
-
-          if sock.closed?
-            puts 'sock already closed'
-            next
-          end
-
           info = @infos[ sock ]
 
           if mon.readable?
-            case info[ :role ]
-            when :roomd
-              # begin
-              #   room, addr = sock.accept_nonblock
-              # rescue IO::WaitReadable, Errno::EINTR
-              #   next
-              # end
-              data, addrinfo, rflags, *controls = sock.recvmsg
+            data, addrinfo, rflags, *controls = sock.recvmsg
+            pcur, ctl_num = data[ 0, 5 ].unpack( 'NC' )
 
-              # room.setsockopt( Socket::SOL_TCP, Socket::TCP_NODELAY, 1 )
-              # @writes[ room ] = ''
+            if pcur != 0
+              next
+            end
 
-              ctl_num = data[ 0 ].unpack( 'C' ).first
+            if ctl_num == 1
+              # 1. p1 to roomd: i'm alive -> custom head
+              now = Time.new
+              p1_addr = addrinfo.to_sockaddr
 
-              if ctl_num == 1
-                # 1 p1 to roomd: heartbeat
-                now = Time.new
-                ip_port = addrinfo.ip_unpack
-                client = info[ :p1s ][ ip_port ]
+              if info[ :p1s ].include?( p1_addr )
+                info[ :p1s ][ p1_addr ][ :timestamp ] = Time.new
+              else
+                filename = save_tmpfile( data[ 5..-1 ], addrinfo, @tmp_dir )
 
-                if info[ :p1s ].include?( ip_port )
-                  info[ :p1s ][ ip_port ][ 1 ] = Time.new
-                else
-                  filename = [ ip_port.join( ':' ), data ].join( '-' )
-                  tmp_path = File.join( @tmp_dir, filename )
-
-                  begin
-                    File.open( tmp_path, 'w' )
-                  rescue Errno::ENOENT, ArgumentError => e
-                    puts "save p1 tmp file #{ ip_port.inspect } #{ e.class }"
-                    next
-                  end
-
-                  info[ :p1s ][ ip_port ] = [ tmp_path, now ]
-                end
-              elsif ctl_num == 2
-                # p2 to roomd: p1 address
-              end
-
-
-            when :room
-
-              if data[ 0, 4 ] == 'come' # connect me!
-                p2_info = @infos[ mon ]
-
-                ip_port = data[ 4..-1 ]
-                p1_room_mon, p1_info = @infos.find{ |_, info| info[ :ip_port ] == ip_port }
-
-                unless p1_info
-                  sock.setsockopt( Socket::SOL_SOCKET, Socket::SO_LINGER, [ 1, 0 ].pack( 'ii' ) )
-                  close_mon( mon )
+                unless filename
                   next
                 end
 
-                @writes[ p1_room_mon.io ] << p2_info[ :ip_port ]
-                p1_room_mon.add_interest( :w )
-
-                begin
-                  File.delete( p1_info[ :tmp_path ] )
-                rescue Errno::ENOENT
-                end
-
-                begin
-                  File.delete( p2_info[ :tmp_path ] )
-                rescue Errno::ENOENT
-                end
-
-                @infos.delete( p1_room_mon )
-                @infos.delete( mon )
-              else
-                puts 'ghost?'
+                info[ :p1s ][ p1_addr ] = {
+                  filename: filename,
+                  timestamp: now
+                }
               end
+            elsif ctl_num == 2
+              # 2. p2 to roomd: tell p1, hole me -> p1 sockaddr
+              p1_addr = data[ 5, 16 ]
+
+              unless @room_info[ :p1s ].include?( p1_addr )
+                puts 'p1 not found'
+                next
+              end
+
+              # send: 4. roomd to p1: p2's hole -> p2 sockaddr
+              @roomd.sendmsg( [ [ 0, 4 ].pack( 'NC' ), addrinfo.to_sockaddr ].join, 0, p1_addr )
+            elsif ctl_num == 7
+              # 7. p1 to roomd: i'm fin
+              sockaddr = addrinfo.to_sockaddr
+              p1_info = @roomd_info[ :p1s ][ sockaddr ]
+
+              unless p1_info
+                puts 'p1 not found'
+                next
+              end
+
+              del_p1( sockaddr, p1_info )
             end
-          end
-
-          if mon.writable?
-            if sock.closed?
-              next
-            end
-
-            data = @writes[ sock ]
-
-            begin
-              written = sock.write_nonblock( data )
-            rescue IO::WaitWritable, Errno::EINTR, IO::WaitReadable
-              next
-            rescue Exception => e
-              close_mon( mon )
-              next
-            end
-
-            @timestamps[ mon ] = Time.new
-            @writes[ sock ] = data[ written..-1 ]
-
-            unless @writes[ sock ].empty?
-              next
-            end
-
-            mon.remove_interest( :w )
           end
         end
       end
     end
 
     def quit!
-      @roles.each{ | mon, _ | mon.io.close }
-      @infos.each do | _, info |
-        begin
-          File.delete( info[ :tmp_path ] )
-        rescue Errno::ENOENT
+      @mutex.synchronize do
+        @roomd_info[ :p1s ].each do | p1_sockaddr, p1_info |
+          del_tmpfile( p1_info[ :filename ] )
         end
       end
 
@@ -180,24 +140,51 @@ module P2p2
 
     private
 
-    def close_mon( mon )
-      sock = mon.io
-      sock.close
+    def loop_expire
+      Thread.new do
+        loop do
+          now = Time.new
 
-      @writes.delete( sock )
-      @selector.deregister( sock )
-      @roles.delete( mon )
-      info = @infos.delete( mon )
+          @mutex.synchronize do
+            @roomd_info[ :p1s ].select{ | _, p1_info | now - p1_info[ :timestamp ] > 7200 }.each do | p1_sockaddr, p1_info |
+              del_p1( p1_sockaddr, p1_info )
+            end
+          end
 
-      if info
-        begin
-          File.delete( info[ :tmp_path ] )
-        rescue Errno::ENOENT
+          sleep 3600
         end
       end
-
-      @timestamps.delete( mon )
     end
 
+    def save_tmpfile( data, addrinfo, tmp_dir )
+      filename = addrinfo.ip_unpack.join( ':' )
+
+      unless data.empty?
+        filename = [ filename, data ].join( '-' )
+      end
+
+      path = File.join( tmp_dir, filename )
+
+      begin
+        File.open( path, 'w' )
+      rescue Errno::ENOENT, ArgumentError => e
+        puts "save p1 tmpfile #{ path.inspect } #{ e.class }"
+        filename = nil
+      end
+
+      filename
+    end
+
+    def del_p1( p1_sockaddr, p1_info )
+      del_tmpfile( p1_info[ :filename ] )
+      @roomd_info[ :p1s ].delete( p1_sockaddr )
+    end
+
+    def del_tmpfile( filename )
+      begin
+        File.delete( File.join( @tmp_dir, filename ) )
+      rescue Errno::ENOENT
+      end
+    end
   end
 end
