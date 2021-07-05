8b09f05ffd89505de50818f787363e450fe9bc9c
takafan <takafan@163.com>
2021-07-05 15:44:10 +0800

    renew ctl interval

--- lib/p2p2/head.rb
+++ lib/p2p2/head.rb
@@ -2,7 +2,7 @@ module P2p2
   READ_SIZE             = 1024 * 1024      # 一次读多少
   WBUFF_LIMIT           = 50 * 1024 * 1024 # 写前上限，超过上限暂停读
   RESUME_BELOW          = WBUFF_LIMIT / 2  # 降到多少以下恢复读
-  RENEW_CTL_INTERVAL    = 60               # p1心跳间隔
+  RENEW_CTL_INTERVAL    = 10               # p1心跳间隔，部分光猫udp端口映射过期时间只有10秒
   CHECK_STATE_INTERVAL  = 1                # 检查过期，恢复读
   EXPIRE_AFTER          = 86400            # 多久没有新流量，过期
   ROOM_TITLE_LIMIT      = 16               # 房间名称字数
